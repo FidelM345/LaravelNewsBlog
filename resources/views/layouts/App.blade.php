2fde5e4bdf4157210403b4c6fe1df9432690e5b4
Fidel <fidelomolo7@gmail.com>
2019-06-22 12:40:28 +0300

    secure assest

--- resources/views/layouts/App.blade.php
+++ resources/views/layouts/App.blade.php
@@ -7,14 +7,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Scripts -->
-    <script src="{{asset('js/app.js') }}"></script>
+    <script src="{{secure_asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    
+    <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
     
 </head>
 <body>
